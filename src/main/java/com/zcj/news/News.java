e8717ef6fb491dbf0ce9b03f15b8aff8952b3980
lkl029 <lkl029@github.com>
2022-10-09 11:49:28 +0800

    push commit

--- src/main/java/com/zcj/news/News.java
+++ src/main/java/com/zcj/news/News.java
@@ -8,5 +8,6 @@ public class News {
         System.out.println("333333");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
