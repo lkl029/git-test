1d8c3d71c46ef91ca574aaa10aa5e9cb7bc2f604
lkl029 <lkl029@github.com>
2022-10-09 10:56:27 +0800

    third commit

--- src/main/java/com/zcj/news/News.java
+++ src/main/java/com/zcj/news/News.java
@@ -3,6 +3,7 @@ package com.zcj.news;
 public class News {
 
     public static void main(String[] args) {
-            System.out.println("mmmm");
+        System.out.println("mmmm");
+        System.out.println("222222");
     }
 }
